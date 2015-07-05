3400d0c4486f01ba670a939c2db6e16d9d636da1
Thomas Kekeisen <thomas@thomaskekeisen.de>
2015-07-05 11:03:14 +0200

    Added more (kinda all possible) colors

--- B_colors.ino
+++ B_colors.ino
@@ -1,7 +1,11 @@
-#define COLOR_BLACK matrix.Color333(0,   0,   0)
-#define COLOR_BLUE  matrix.Color333(0,   0,   255)
-#define COLOR_GREEN matrix.Color333(0,   255, 0)
-#define COLOR_RED   matrix.Color333(255, 0,   0)
+#define COLOR_BLACK     matrix.Color333(0,   0,   0)
+#define COLOR_BLUE      matrix.Color333(0,   0,   255)
+#define COLOR_GREEN     matrix.Color333(0,   255, 0)
+#define COLOR_PURPLE    matrix.Color333(255, 0,   255)
+#define COLOR_RED       matrix.Color333(255, 0,   0)
+#define COLOR_TURQIUOSE matrix.Color333(0,   255, 255)
+#define COLOR_WHITE     matrix.Color333(255, 255, 255)
+#define COLOR_YELLOW    matrix.Color333(255, 255, 0)
 
 #define DIRECTION_UP    0
 #define DIRECTION_DOWN  1
